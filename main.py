02bc291d96a6dca629523747a69396f47e04c224
Jakub Matuszczak <kubamatuszczak2@gmail.com>
2024-07-11 01:51:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,37 @@ def download(url):
     page.locator('#input-query').fill(url)
     page.locator("#btn-download").click()
 
-    #get the url of button
-    # url1 = page.locator("#button-download-ready > a:nth-child(2)").get_attribute("href")
-    # print(url1)
-    # url2 = page.locator("#button-download-ready > a:nth-child(4)").get_attribute("href")
-    # print(url2)
+    urls = []
+    for i in range(2, 20, 2):
+        try:
+            if "SLIDE" in page.locator(f"#button-download-ready > a:nth-child({i})").text_content():
+                url = page.locator(f"#button-download-ready > a:nth-child({i})").get_attribute("href")
+                if url not in urls:
+                    urls.append(url)
+            else:
+                break
+        except:
+            break
+    browser.close()
+    playwright.stop()
+    print("Znaleziono "+str(len(urls))+" zdjęć")
+    print("Pobieranie zdjęć...")
 
     time.sleep(3)
+    random_int = random.randint(0, 1000)
+
+    if sys.platform == 'darwin':
+        print("KORZYSTASZ Z MACOS")
+        os.mkdir(str(random_int))
+        for i in range(len(urls)):
+            urlretrieve(urls[i], f"{random_int}/{i}.jpg")
+    else:
+        base_filepath = os.getcwd()
+        print("KORZYSTASZ Z TEGO GORSZEGO SYSTEMU")
+        print("KORZYSTASZ Z TEGO GORSZEGO SYSTEMU")
+        print("KORZYSTASZ Z TEGO GORSZEGO SYSTEMU")
+        os.mkdir(f"{base_filepath}\\{random_int}")
+        for i in range(len(urls)):
+            urlretrieve(urls[i], f"{base_filepath}\\{random_int}\\{i}.jpg")
+    print("Pobieranie zakończone")
+    print("Zdjęcia znajdziesz w folderze "+str(random_int))
